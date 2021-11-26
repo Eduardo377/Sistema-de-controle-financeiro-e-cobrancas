35f925313d821f79c94cefc67829fabb98bc19aa
Daniel <daniel.repres@hotmail.com>
2021-11-26 16:29:44 -0300

    Ajustes nos endpoints e YOU

--- src/validacoes/editarUsuarioSchema.js
+++ src/validacoes/editarUsuarioSchema.js
@@ -1,17 +1,11 @@
 const yup = require('./yup');
 
 const editarUsuarioSchema = yup.object().shape({
-    email: yup
-        .string()
-        .email(),
-    senha: yup
-        .string(),
-    nome: yup
-        .string(),
-    cpf: yup
-        .string(),
-    tel: yup
-        .string()
+    email: yup.string().email(),
+    senha: yup.string().min(5).max(10),
+    nome: yup.string().min(3).max(10),
+    cpf: yup.string(),
+    tel: yup.string()
 });
 
 module.exports = editarUsuarioSchema;
