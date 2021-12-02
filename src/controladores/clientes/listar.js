bcf6aa381f0fbb40553d9aa1b1882c4312f6a99c
Eduardo377 <eduardogomes377@gmail.com>
2021-12-02 16:30:14 -0300

    revisao do endpoint - listar cobrancas

--- src/controladores/clientes/listar.js
+++ src/controladores/clientes/listar.js
@@ -1,8 +1,12 @@
 const knex = require('../../conexao');
 
 const listarClientes = async(req, res) => {
-    const clientes = await knex('clientes').returning('*');
-    return res.status(200).json(clientes);
+    try {
+        const clientes = await knex('clientes').returning('*');
+        return res.status(200).json(clientes);
+    } catch (error) {
+        return res.status(404).json(error.message);
+    }
 }
 
 module.exports = { listarClientes };
